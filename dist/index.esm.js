1e19cbc212c113c5884b67df7d8ab6dc70a8be3d
Manuel0420 <leandro.guevarra0420@gmail.com>
2021-03-04 13:56:14 +0800

    replaced goose pics to mufasa pics

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -343,7 +343,7 @@ var Icon$p = function (props) {
 
 var Icon$q = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 32 32" }, props),
-        React.createElement("image", { width: "32", height: "32", href: "images/egg/logo.png" })));
+        React.createElement("image", { width: "32", height: "32", href: "images/mufasabnb.png" })));
 };
 
 var Icon$r = function (props) {
@@ -400,7 +400,7 @@ var Icon$u = function (props) {
 
 var Icon$v = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 90 90" }, props),
-        React.createElement("image", { width: 90, height: 90, href: "images/egg/9.png" })));
+        React.createElement("image", { width: 90, height: 90, href: "images/mufasabnb.png" })));
 };
 
 var Icon$w = function (props) {
@@ -1860,7 +1860,7 @@ var Icon$O = function (props) {
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
     return (React.createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
-        React.createElement("image", { width: "205", height: "26", href: isDark ? 'images/egg/LogoTextNewDark.png' : 'images/egg/LogoTextNewWhite.png' })));
+        React.createElement("image", { width: "205", height: "26", href: isDark ? 'images/mufasabnb.png' : 'images/mufasabnb.png' })));
 };
 
 var Icon$P = function (props) {
