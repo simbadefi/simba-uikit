6a13d1b8cc552a473c75838816b39f84f4019163
Manuel0420 <leandro.guevarra0420@gmail.com>
2021-03-14 09:36:19 +0800

    fixed logo

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -343,7 +343,7 @@ var Icon$p = function (props) {
 
 var Icon$q = function (props) {
     return (React.createElement(Svg, __assign({ viewBox: "0 0 32 32" }, props),
-        React.createElement("image", { width: "32", height: "32", href: "images/logo.svg" })));
+        React.createElement("image", { width: "32", height: "32", href: "images/mufasabnb.png" })));
 };
 
 var Icon$r = function (props) {
